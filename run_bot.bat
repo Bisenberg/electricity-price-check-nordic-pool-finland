9338c57336769b4f4892ab6825216fbdb6b89127
Bisenberg <tresclub33@gmail.com>
2025-11-03 14:37:58 +0200

    Spelling mistake fixes

--- run_bot.bat
+++ run_bot.bat
@@ -3,4 +3,4 @@ cd /d "D:\Projektit\Pricecheck"
 echo [%date% %time%] Starting >> elec_log.txt
 call venv\Scripts\activate
 python bot_fetch.py >> elec_log.txt 2>&1
-echo [%date% %time%] Finising >> elec_log.txt
+echo [%date% %time%] Finishing >> elec_log.txt
